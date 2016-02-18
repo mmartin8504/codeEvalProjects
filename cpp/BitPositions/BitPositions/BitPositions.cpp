dc8901ae5d8087d4fde9fb0414384ca9bb612464
Mitchell Martin <mmartin8504@gmail.com>
2016-02-18 15:33:06 -0500

    Clean up some newlines/spacing/projects

--- cpp/BitPositions/BitPositions/BitPositions.cpp
+++ cpp/BitPositions/BitPositions/BitPositions.cpp
@@ -6,30 +6,30 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    ifstream stream(argv[1]);
+	ifstream stream(argv[1]);
 	string line;
 	string delimeter = ",";
 	string tokens[3];
-    while (getline(stream, line)) {
-		size_t pos = 0;
-		int i = 0;
-		while ((pos = line.find(delimeter)) != string::npos) {
-			tokens[i] = line.substr(0, pos);
-			line = line.substr(pos + delimeter.length());
-			++i;
-		}
-		tokens[i] = line;
-		int num, p1, p2;
-		num = stoi(tokens[0]);
-		p1 = stoi(tokens[1]);
-		p2 = stoi(tokens[2]);
+	while (getline(stream, line)) {
+	size_t pos = 0;
+	int i = 0;
+	while ((pos = line.find(delimeter)) != string::npos) {
+		tokens[i] = line.substr(0, pos);
+		line = line.substr(pos + delimeter.length());
+		++i;
+	}
+	tokens[i] = line;
+	int num, p1, p2;
+	num = stoi(tokens[0]);
+	p1 = stoi(tokens[1]);
+	p2 = stoi(tokens[2]);
 
-		int bit1 = (num & (1<<p1-1)) >> p1-1;
-		int bit2 = (num & (1<<p2-1)) >> p2-1;
-		if (!(bit1 ^ bit2))
-			cout << "true" << endl;
-		else
-			cout << "false" << endl;
-    }
-    return 0;
+	int bit1 = (num & (1<<p1-1)) >> p1-1;
+	int bit2 = (num & (1<<p2-1)) >> p2-1;
+	if (!(bit1 ^ bit2))
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+	}
+	return 0;
 }
