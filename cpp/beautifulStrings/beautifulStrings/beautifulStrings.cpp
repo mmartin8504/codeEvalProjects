6ad8da7bccde58544b4610cf52952b6283aa7f14
Mitchell Martin <mmartin8504@gmail.com>
2016-04-25 11:22:18 -0400

    Push some updates to old projects. Added comments, new stuff for details.

--- cpp/beautifulStrings/beautifulStrings/beautifulStrings.cpp
+++ cpp/beautifulStrings/beautifulStrings/beautifulStrings.cpp
@@ -16,29 +16,40 @@ int main(int argc, char *argv[]) {
     string line;
 
     while (getline(stream, line)) {
-		char max = '\0';
+        // count the number of unique letters
 		map<char, int> charCount;
 		int score = 0;
+        // look at each character
 		for (string::size_type i=0; i<line.length(); ++i)
 		{
+            // don't care about case
 			char c = toupper(line[i]);
 			line[i] = c;
+            // only care about letters
 			if (isalpha(c))
 			{
 				pair<map<char,int>::iterator,bool> ret;
+                // Insert returns a pair, ret.second is true if a new element was inserted
 				ret = charCount.insert(pair<char, int>(c,1));
+                // if that wasn't a new element, increment the existing count
 				if (!ret.second)
 					ret.first->second += 1;
 			}
 		}
 		
+        // Get the counts of each letter. We don't really care about which letter had the most/least
+        // just the total number of each.
 		vector<int> values;
 		for (map<char,int>::iterator it=charCount.begin(); it!=charCount.end(); ++it)
 		{
 			values.push_back(it->second);
 		}
 
+        // Sort the counts (ascending)
 		sort(values.begin(), values.end());
+
+        // To max the score, the letter with the highest count gets the highest possible score, and
+        // so on as the counts decrease
 		int multiplier = 26;
 		for (vector<int>::reverse_iterator it=values.rbegin(); it!=values.rend(); ++it, --multiplier)
 		{
--- cpp/details/details/details.cpp
+++ cpp/details/details/details.cpp
@@ -6,42 +6,97 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int solveSplit(string line)
 {
-	ifstream stream(argv[1]);
-	string line;
 	string delimeter = ",";
 
-	while (getline(stream, line))
+	size_t pos = 0;
+	vector<string> rows;
+	while ((pos = line.find(delimeter)) != string::npos)
 	{
-		// Do something with the line
-		size_t pos = 0;
-		vector<string> rows;
-		while ((pos = line.find(delimeter)) != string::npos)
+		// split on delimiter
+		rows.push_back(line.substr(0, pos));
+		line = line.substr(pos + delimeter.length());
+	}
+	// add each row to the vector
+	rows.push_back(line);
+
+	// for each row, find the space between the first . and the first Y
+	int closestPos = rows[0].length();
+	for (vector<string>::size_type i = 0; i < rows.size(); ++i)
+	{
+		int spaceBetween = 0;
+		for (string::size_type j = 0; j < rows[i].length(); ++j)
 		{
-			rows.push_back(line.substr(0, pos));
-			line = line.substr(pos + delimeter.length());
-		}
-		rows.push_back(line);
-	
-		int closestPos = rows[0].length();
-		for (vector<string>::size_type i = 0; i < rows.size(); ++i)
-		{
-			int spaceBetween = 0;
-			for (int j = 0; j < rows[i].length(); ++j)
+			if (spaceBetween < closestPos)
 			{
 				if (rows[i][j] == '.')
 					++spaceBetween;
 				else if (rows[i][j] == 'Y')
 				{
+					// update closestPos if space is less than current closest
 					if (closestPos >= spaceBetween)
 						closestPos = spaceBetween;
-					continue;
+					break;
 				}
 			}
+			else
+				break; // go ahead and stop looking at this row if we've exceeded closestPos
 		}
-		cout << closestPos << endl;
 	}
 
+	return closestPos;
+}
+
+int solveNoSplit(string line)
+{
+	int lineLen = line.length();
+	int closestPos = lineLen;
+	int spaceBetween = 0;
+	bool keepCounting = true;
+	for (int i = 0; i < lineLen; ++i)
+	{
+		// Handle the case X....Y,X..Y.Y
+		if (keepCounting && line[i] == 'Y')
+		{
+			// need to stop counting . after the first Y
+			// but also don't come in here if keepCounting is
+			// false since Y can be the last character and we
+			// need to go into the else if on the last char
+			// to update closestPos for the last row in the line
+			keepCounting = false;
+		}
+		else if (keepCounting && line[i] == '.')
+		{
+			// count the . if we're still doing that
+			++spaceBetween;
+		}
+		else if (line[i] == ',' || i == lineLen-1) // end of a row OR the end of the line
+		{
+			// if the space we just counted was less than our closest,
+			// update the closest value
+			if (spaceBetween < closestPos)
+				closestPos = spaceBetween;
+			// reset the space counter for the next row
+			spaceBetween = 0;
+			// we need to start counting . again
+			keepCounting = true;
+		}
+	}
+	
+	return closestPos;
+}
+
+int main(int argc, char *argv[])
+{
+	ifstream stream(argv[1]);
+	string line;
+
+	while (getline(stream, line))
+	{
+		int ans = solveNoSplit(line);
+		//int ans = solveSplit(line);
+		cout << ans << endl;
+	}
 	return 0;
 }
